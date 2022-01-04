57ef0cd2407f6b18561d431a66a1ec4d67c450bc
Ing-Ram <chad117gram@gmail.com>
2022-01-03 23:44:56 -0800

    Added codepath Asynch. Created Movie model to organize returned json data.

--- app/src/main/java/com/codepath/ingram/flixster/MainActivity.kt
+++ app/src/main/java/com/codepath/ingram/flixster/MainActivity.kt
@@ -2,10 +2,43 @@ package com.codepath.ingram.flixster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+import org.json.JSONException
 
+const val TAG = "MAIN ACTIVITY"
+private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
 class MainActivity : AppCompatActivity() {
+    private val movies = mutableListOf<Movie>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = AsyncHttpClient()
+
+        client.get(NOW_PLAYING_URL,object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int, headers: Headers?, response: String?,throwable: Throwable?
+            ) {
+                Log.e(TAG, "onFailure")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG,"onSuccess: JSON data $json")
+                try {
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    Log.i(TAG,"Movie List $movies")
+                } catch (e: JSONException) {
+                    Log.e(TAG, "ENCOUNTERED EXCEPTION $e")
+
+                }
+
+            }
+
+        })
+
     }
 }
